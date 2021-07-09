54de6fad595dfc10717b141b5e15f682d8f423b1
Camilo <camilo@ninjas.cl>
2021-07-08 23:14:01 -0400

    removed unused dir

--- site/templates/_mix.php
+++ site/templates/_mix.php
@@ -1,7 +1,7 @@
-<?php // automatically generated at 2021-07-09T02:12:19.590Z by: npm run dev 
+<?php // automatically generated at 2021-07-09T03:13:35.446Z by: npm run dev 
 
 class LaravelMixAssets {
-    const version = '2021-07-09T02:12:19.590Z';
+    const version = '2021-07-09T03:13:35.446Z';
     const files = [
         "/js/app.js" => "/js/app.js?id=ac73151db2fa5767fa56",
         "/css/app.css" => "/css/app.css?id=c216e991b1b780eda9ac",
